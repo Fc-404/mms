4b31c019cbe191f1f246964b9c56e8fa13a7200f
gjl <2601721443@qq.com>
2025-02-24 13:40:52 +0800

    修复edit

--- app/(home)/materials/edit/page.tsx
+++ app/(home)/materials/edit/page.tsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 import { Suspense, useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 
-export default function EditMaterial() {
+function EditMaterial() {
   const labelClass =
     "text-[1.1rem] w-20 mr-4 block md:inline-block md:text-right";
   const inputClass = `inline-block h-9 w-full md:w-auto transition-all
@@ -91,163 +91,169 @@ export default function EditMaterial() {
   };
 
   return (
-    <Suspense>
-      <div>
+    <div>
+      <Link href="/materials">
+        <button
+          className="text-red-500 inline-block py-1 pr-3 pl-2 mx-4
+        rounded-lg transition-all duration-200 ease-in-out hover:bg-white"
+        >
+          <IoIosArrowBack className="inline mb-[0.2rem]" />
+          返回
+        </button>
+      </Link>
+      <h1 className="text-2xl font-bold inline-block text-neutral-600">
+        {paramUrl.get("type") === "add" ? "添加物料" : "编辑物料"}
+      </h1>
+      <form className="flex flex-col flex-wrap md:h-[22rem] p-6 mr-12 md:mt-12 content-around">
+        <div className="w-full md:w-auto">
+          <label className={labelClass}>
+            <span className="text-red-500">*</span> 名称
+          </label>
+          <input
+            name="name"
+            className={inputClass}
+            type="text"
+            placeholder=""
+            value={materialInfo.name}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>
+            <span className="text-red-500">*</span> 值
+          </label>
+          <input
+            name="value"
+            className={inputClass}
+            type="text"
+            placeholder=""
+            value={materialInfo.value}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>
+            <span className="text-red-500">*</span> 数量
+          </label>
+          <input
+            min={0}
+            name="amount"
+            className={inputClass}
+            type="number"
+            placeholder=""
+            value={materialInfo.amount}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>
+            <span className="text-red-500">*</span> 封装
+          </label>
+          <input
+            name="package"
+            className={inputClass}
+            type="text"
+            placeholder=""
+            value={materialInfo.package}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>类型</label>
+          <input
+            name="class"
+            className={inputClass}
+            type="text"
+            placeholder=""
+            value={materialInfo.class}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>型号</label>
+          <input
+            name="model"
+            className={inputClass}
+            type="text"
+            placeholder=""
+            value={materialInfo.model}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>厂家</label>
+          <input
+            name="manufacturer"
+            className={inputClass}
+            type="text"
+            placeholder=""
+            value={materialInfo.manufacturer}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>描述</label>
+          <input
+            className={`${inputClass} md:inline hidden`}
+            type="text"
+            placeholder=""
+          />
+          <textarea
+            name="description"
+            className={`${inputClass} 
+              md:hidden block min-h-9 max-h-32`}
+            placeholder=""
+            value={materialInfo.description}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>封面</label>
+          <input
+            name="avatar"
+            className={inputClass}
+            type="text"
+            placeholder=""
+            value={materialInfo.avatar}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+        <div>
+          <label className={labelClass}>数据手册</label>
+          <input
+            name="datasheet"
+            className={inputClass}
+            type="text"
+            placeholder=""
+            value={materialInfo.datasheet}
+            onChange={updateMaterialInfo}
+          />
+        </div>
+      </form>
+
+      <div className="fixed bottom-12 right-12">
+        <button
+          onClick={addSubmit}
+          className={`${butClass} bg-red-400 text-neutral-100 hover:bg-red-500`}
+        >
+          添加
+        </button>
         <Link href="/materials">
           <button
-            className="text-red-500 inline-block py-1 pr-3 pl-2 mx-4
-        rounded-lg transition-all duration-200 ease-in-out hover:bg-white"
+            className={`${butClass} bg-neutral-200 text-red-400 hover:bg-white`}
           >
-            <IoIosArrowBack className="inline mb-[0.2rem]" />
-            返回
+            退出
           </button>
         </Link>
-        <h1 className="text-2xl font-bold inline-block text-neutral-600">
-          {paramUrl.get("type") === "add" ? "添加物料" : "编辑物料"}
-        </h1>
-        <form className="flex flex-col flex-wrap md:h-[22rem] p-6 mr-12 md:mt-12 content-around">
-          <div className="w-full md:w-auto">
-            <label className={labelClass}>
-              <span className="text-red-500">*</span> 名称
-            </label>
-            <input
-              name="name"
-              className={inputClass}
-              type="text"
-              placeholder=""
-              value={materialInfo.name}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>
-              <span className="text-red-500">*</span> 值
-            </label>
-            <input
-              name="value"
-              className={inputClass}
-              type="text"
-              placeholder=""
-              value={materialInfo.value}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>
-              <span className="text-red-500">*</span> 数量
-            </label>
-            <input
-              min={0}
-              name="amount"
-              className={inputClass}
-              type="number"
-              placeholder=""
-              value={materialInfo.amount}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>
-              <span className="text-red-500">*</span> 封装
-            </label>
-            <input
-              name="package"
-              className={inputClass}
-              type="text"
-              placeholder=""
-              value={materialInfo.package}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>类型</label>
-            <input
-              name="class"
-              className={inputClass}
-              type="text"
-              placeholder=""
-              value={materialInfo.class}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>型号</label>
-            <input
-              name="model"
-              className={inputClass}
-              type="text"
-              placeholder=""
-              value={materialInfo.model}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>厂家</label>
-            <input
-              name="manufacturer"
-              className={inputClass}
-              type="text"
-              placeholder=""
-              value={materialInfo.manufacturer}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>描述</label>
-            <input
-              className={`${inputClass} md:inline hidden`}
-              type="text"
-              placeholder=""
-            />
-            <textarea
-              name="description"
-              className={`${inputClass} 
-              md:hidden block min-h-9 max-h-32`}
-              placeholder=""
-              value={materialInfo.description}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>封面</label>
-            <input
-              name="avatar"
-              className={inputClass}
-              type="text"
-              placeholder=""
-              value={materialInfo.avatar}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-          <div>
-            <label className={labelClass}>数据手册</label>
-            <input
-              name="datasheet"
-              className={inputClass}
-              type="text"
-              placeholder=""
-              value={materialInfo.datasheet}
-              onChange={updateMaterialInfo}
-            />
-          </div>
-        </form>
-
-        <div className="fixed bottom-12 right-12">
-          <button
-            onClick={addSubmit}
-            className={`${butClass} bg-red-400 text-neutral-100 hover:bg-red-500`}
-          >
-            添加
-          </button>
-          <Link href="/materials">
-            <button
-              className={`${butClass} bg-neutral-200 text-red-400 hover:bg-white`}
-            >
-              退出
-            </button>
-          </Link>
-        </div>
       </div>
+    </div>
+  );
+}
+
+export default function EditMaterialSuspense() {
+  return (
+    <Suspense>
+      <EditMaterial />
     </Suspense>
   );
 }
